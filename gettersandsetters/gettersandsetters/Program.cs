d2844bd2e87a684b8448eee8c684430286e5d0d1
m-h-d-salih <salihsha656@gmail.com>
2024-08-23 12:14:01 +0530

    getters and setters

---
+++ gettersandsetters/gettersandsetters/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace gettersandsetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(100);
+            Console.WriteLine(car.Speed);
+            car.Speed = 100000;
+            Console.WriteLine(car.Speed);
+        }
+    }
+    public class Car
+    {
+        private int speed;
+        public Car(int speed)
+        {
+            this.speed = speed;
+        }
+        public int Speed
+        {
+            get { return speed; }
+            set
+            {
+                if (value > 3000)
+
+                    speed = 300;
+                else
+                    speed = value;
+
+
+
+
+            }
+        }
+    }
+}
