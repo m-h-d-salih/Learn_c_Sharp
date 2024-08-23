24c4aa3ba1ee70d5e31ff2b8814a239f68152408
m-h-d-salih <salihsha656@gmail.com>
2024-08-23 14:07:45 +0530

    auto implementaion in getter and setter

--- gettersandsetters/gettersandsetters/Program.cs
+++ gettersandsetters/gettersandsetters/Program.cs
@@ -8,6 +8,11 @@
             Console.WriteLine(car.Speed);
             car.Speed = 100000;
             Console.WriteLine(car.Speed);
+            //auto implementaion
+            Car car1 = new Car("hummer");
+            Console.WriteLine(car1.Model);
+            car1.Model = "mclaren";
+            Console.WriteLine(car1.Model);
         }
     }
     public class Car
@@ -27,11 +32,13 @@
                     speed = 300;
                 else
                     speed = value;
-
-
-
-
             }
         }
+        public string Model { get; set; }//auto implementaion => mainly used in if there is no logic required
+        public Car(string model)
+        {
+            this.Model = model;
+        }
+       
     }
 }
