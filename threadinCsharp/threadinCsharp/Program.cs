5464d29fcaa1ab401d4a1a5535b6c73c6cc4066b
m-h-d-salih <salihsha656@gmail.com>
2024-08-23 14:31:00 +0530

    multi thread in c sharp

---
+++ threadinCsharp/threadinCsharp/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace threadinCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread=Thread.CurrentThread;
+            //Thread thread1 = new Thread(countup);
+            Thread thread2 = new Thread(countup);
+            Thread thread1 = new Thread(()=>countdown("counter down"));//suppose we need to pass parameter
+            thread.Name = "main thread";
+            //countup();
+            //countdown();
+            thread1.Start();
+            thread2.Start();
+            Console.WriteLine($"{thread.Name} completed");
+        }
+        public static void countup()
+        {
+            for(int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"counter up {i} seconds");
+                Thread.Sleep(1000);
+            }
+        }
+        public static void countdown(string countername)
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine($"{countername} {i} seconds");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
