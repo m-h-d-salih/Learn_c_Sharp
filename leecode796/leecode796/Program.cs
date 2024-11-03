76638bfe205ca720a0788cfdf8ccdd975be8aa0e
m-h-d-salih <salihsha656@gmail.com>
2024-11-03 15:16:32 +0530

    leetcode 796

---
+++ leecode796/leecode796/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace leecode796
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RotateString("abcde","bcdea"));
+
+        }
+            public static bool RotateString(string s, string goal)
+            {
+                int count = 0;
+                while (count < s.Length)
+                {
+                    if (s == goal) return true;
+                    s = s.Substring(1) + s[0];
+                    count++;
+                }
+                return false;
+            }
+        }
+    }
+
