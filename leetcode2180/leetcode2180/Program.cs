2079af39c82f1de2796e6c9bd3ca301c8ccdab4f
m-h-d-salih <salihsha656@gmail.com>
2024-11-04 18:11:05 +0530

    leetcode 2180

---
+++ leetcode2180/leetcode2180/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode2180
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountEven(30));
+        }
+        public static int CountEven(int num)
+        {
+            int count = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                string arr = i.ToString();
+                int sum = 0;
+                foreach (char item in arr)
+                {
+                    sum += int.Parse(item.ToString());
+                }
+                if (sum % 2 == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+        
+}
