73a02876f54d1603514e587101a91d7907578f53
m-h-d-salih <salihsha656@gmail.com>
2024-11-05 15:16:23 +0530

    leetcode-2914

---
+++ leetcode-qst-2914/leetcode-qst-2914/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_qst_2914
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinChanges("1001"));
+        }
+
+            public static int MinChanges(string s)
+            {
+                int count = 0;
+                for (int i = 1; i < s.Length; i += 2)
+                {
+                    if (s[i] != s[i - 1])
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+        
+    }
+}
